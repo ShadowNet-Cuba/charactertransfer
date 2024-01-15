b148a5d59b695abac94ffb60c4cc6ef86927636d
qwe900 <qwe900@web.de>
2024-01-15 13:23:45 +0100

    Update WoW Modelviewer
    Cors Proxy is Neccesary

--- js/wow_model_viewer.js
+++ js/wow_model_viewer.js
@@ -1,22 +1,14 @@
-if (!window.WH) {
-    window.WH = {};
-    // eslint-disable-next-line no-unused-vars
-    window.WH.debug = function (...args) {
-        // console.log(args);
-    };
-    window.WH.defaultAnimation = `Stand`;
-}
+import {getCharacterOptions} from "./character_modeling.js"
 
 
 // eslint-disable-next-line no-undef
 class WowModelViewer extends ZamModelViewer {
-
     /**
      * Returns the list of animation names
      * @returns {Array.<string>}
      */
     getListAnimations() {
-        return [...new Set(this.renderer.models[0].ap.map(e => e.j))];
+        return [...new Set(this.renderer.models[0].ap.map(e => e.j))]
     }
 
     /**
@@ -24,7 +16,7 @@ class WowModelViewer extends ZamModelViewer {
      * @param {number} val
      */
     setDistance(val) {
-        this.renderer.distance = val;
+        this.renderer.distance = val
     }
 
     /**
@@ -33,9 +25,9 @@ class WowModelViewer extends ZamModelViewer {
      */
     setAnimation(val) {
         if (!this.getListAnimations().includes(val)) {
-            console.warn(`${this.constructor.name}: Animation ${val} not found`);
+            console.warn(`${this.constructor.name}: Animation ${val} not found`)
         }
-        this.renderer.models[0].setAnimation(val);
+        this.renderer.models[0].setAnimation(val)
     }
 
     /**
@@ -43,7 +35,7 @@ class WowModelViewer extends ZamModelViewer {
      * @param {boolean} val
      */
     setAnimPaused(val) {
-        this.renderer.models[0].setAnimPaused(val);
+        this.renderer.models[0].setAnimPaused(val)
     }
 
     /**
@@ -51,7 +43,7 @@ class WowModelViewer extends ZamModelViewer {
      * @param {number} val
      */
     setAzimuth(val) {
-        this.renderer.azimuth = val;
+        this.renderer.azimuth = val
     }
 
     /**
@@ -59,7 +51,7 @@ class WowModelViewer extends ZamModelViewer {
      * @param {number} val
      */
     setZenith(val) {
-        this.renderer.zenith = val;
+        this.renderer.zenith = val
     }
 
     /**
@@ -67,7 +59,7 @@ class WowModelViewer extends ZamModelViewer {
      * @return {number}
      */
     getAzimuth() {
-        return this.renderer.azimuth;
+        return this.renderer.azimuth
     }
 
     /**
@@ -75,10 +67,82 @@ class WowModelViewer extends ZamModelViewer {
      * @return {number}
      */
     getZenith() {
-        return this.renderer.zenith;
+        return this.renderer.zenith
+    }
+
+    /**
+     * This methode is based on `updateViewer` from Paperdoll.js (https://wow.zamimg.com/js/Paperdoll.js?3ee7ec5121)
+     *
+     * @param slot {number}: Item slot number
+     * @param displayId {number}: Item display id
+     * @param enchant {number}: Enchant (experimental not tested)
+     */
+    updateItemViewer(slot, displayId, enchant) {
+        const s = window.WH.Wow.Item
+        if (slot === s.INVENTORY_TYPE_SHOULDERS) {
+            // this.method(`setShouldersOverride`, [this.getShouldersOverrideData()]);
+        }
+        const a = (slot === s.INVENTORY_TYPE_ROBE) ? s.INVENTORY_TYPE_CHEST : slot
+
+        window.WH.debug(`Clearing model viewer slot:`, a.toString())
+        this.method(`clearSlots`, slot.toString())
+        if (displayId) {
+            window.WH.debug(`Attaching to model viewer slot:`, slot.toString(), `Display ID:`, displayId, `Enchant Visual:`, enchant)
+            this.method(`setItems`, [[{
+                slot: slot,
+                display: displayId,
+                visual: enchant || 0
+            }]])
+        }
+    }
+
+    setNewAppearance(options) {
+        if(!this.currentCharacterOptions) {
+            throw Error(`Character options are not set`)
+        }
+        const characterOptions = getCharacterOptions(options, this.currentCharacterOptions)
+        const race = this.characterRace
+        const gender = this.characterGender
+        this.method(`setAppearance`, {race: race, gender: gender, options: characterOptions})
     }
 }
 
+// Instance variables
+WowModelViewer.prototype._currentCharacterOptions = 0
+WowModelViewer.prototype._characterGender = null
+WowModelViewer.prototype._characterRace = null
+
+// Getter and Setter for currentCharacterOptions
+Object.defineProperty(WowModelViewer.prototype, `currentCharacterOptions`, {
+    get: function() {
+        return this._currentCharacterOptions
+    },
+    set: function(value) {
+        this._currentCharacterOptions = value
+    }
+})
+
+// Getter and Setter for characterGender
+Object.defineProperty(WowModelViewer.prototype, `characterGender`, {
+    get: function() {
+        return this._characterGender
+    },
+    set: function(value) {
+        this._characterGender = value
+    }
+})
+
+// Getter and Setter for characterRace
+Object.defineProperty(WowModelViewer.prototype, `characterRace`, {
+    get: function() {
+        return this._characterRace
+    },
+    set: function(value) {
+        this._characterRace = value
+    }
+})
+
+
 export {
-    WowModelViewer
-};
+    WowModelViewer,
+}
